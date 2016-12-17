e5784c28119b3b62b2d2ce8e890109c0686e24cc
Tim Kok <timmykok@gmail.com>
2016-12-17 21:14:55 +0000

    Add mass assignment and date formatting to Roaster model

--- app/Roaster.php
+++ app/Roaster.php
@@ -3,8 +3,35 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Roaster extends Model
 {
-    //
+    /**
+     * Attributes that can be mass assigned
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'established_year',
+        'website_url',
+        'store_url',
+        'city',
+        'state',
+        'country',
+        'logo'
+    ];
+
+    protected $dates = ["established_year"];
+
+    /**
+     * Set established_year as Carbon object
+     *
+     * @param $date
+     * @return return type Carbon
+     */
+    public function setEstablishedYearAttribute($date)
+    {
+        $this->attributes['established_year'] = Carbon::createFromDate($date, 1, 1);
+    }
 }
